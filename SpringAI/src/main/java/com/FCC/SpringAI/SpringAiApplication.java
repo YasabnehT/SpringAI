23738666ccdd4d08ec92d5cdcb55a60257c3c86f
YasabnehT <yastesh10@gmail.com>
2024-12-18 14:46:51 +0300

    commandline runnner added

--- SpringAI/src/main/java/com/FCC/SpringAI/SpringAiApplication.java
+++ SpringAI/src/main/java/com/FCC/SpringAI/SpringAiApplication.java
@@ -9,5 +9,12 @@ public class SpringAiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAiApplication.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner renner(ChatClient.Builder builder){
+		return args -> {
+			ChatClient chatClient = builder.build();
+			String response = chatClient.prompt("Tell me a joke").call().content();
+			System.out.println(response);
+		}
+	}
 }
